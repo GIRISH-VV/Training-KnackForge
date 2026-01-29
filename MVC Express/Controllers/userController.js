668144ac3f7c811b1518808bfe267aba9f338a3e
GIRISH-VV <girishkumarvv19@gmail.com>
2026-01-29 19:01:35 +0530

    mvc

--- MVC Express/Controllers/userController.js
+++ MVC Express/Controllers/userController.js
@@ -11,8 +11,9 @@ exports.dashboardPage = (req,res,next) => {
 exports.loginProcess = (req,res,next) => {
     //get user data - model
     const user = getUser(req.body.email)
-    if(user !== null && user.password === req.body.password)
+    if(user !== null && user.password === req.body.password){
         res.redirect('dashboard')
+    }
     else if(user === null){
         res.render('error'),{
             message:'No User exists with this Email'
